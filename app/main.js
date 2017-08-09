d852879bc94a76513eb652767b62c6b6f4cf1333
Pico12 <pico12@free.fr>
2017-08-09 14:23:07 +0200

    ADD : Webpack.

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
-(function(){ // Prevent from being global. Will be removed when using webpack
-    'use strict'
-    const app = angular.module('main', []);
-    app.run(['$rootScope', function($rootScope){
-        $rootScope.name = "you";
-    }])
-})()
+'use strict'
+import 'angular';
+
+const app = angular.module('main', []);
+
+app.run(['$rootScope', function($rootScope){
+    $rootScope.name = "you";
+}])
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = require('express')
+const express = require('express');
 var serveIndex = require('serve-index');
+const app = express();
 
-const app = express()
+// With theses lines, webpack will run at each file saving.
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+webpackConfig.output.path = '/';
+const compiler = webpack(webpackConfig);
+app.use('/app/wpk/', webpackDevMiddleware(compiler, {}));
 
-app.use(express.static('.'))
+// Serve static files
+app.use(express.static('.'));
 
+// Function called when others failed
 app.use(function(req, res, next) {
     console.log('404: Page not Found', req.url);
     next();
 });
 
+// Set listen port
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
+    console.log('Example app listening on port 8000!');
 })
